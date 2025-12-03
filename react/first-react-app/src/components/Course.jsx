14361c8a9ae56b1070d4066f9e20c7b2291ed872
Jagadish6246 <bjp.jagadish@outlook.com>
2025-12-03 18:18:49 +0530

    component topic explanation with courses exmaple

---
+++ react/first-react-app/src/components/Course.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Course(props) {
+  return (
+    <div className='d-flex flex-column border border-2 border-warning p-2 m-5 rounded shadow text-center'
+     style={{width:"350px", height:"250px"}}>
+      <h4>{props.children}</h4>
+      <b>Price: {props.price}</b>
+      <b> Trainer: <i>{props.trainer}</i></b>
+      <h3>{props.duration}</h3>
+      <p>{props.desc}</p>
+    </div>
+  )
+}
+
+export default Course
