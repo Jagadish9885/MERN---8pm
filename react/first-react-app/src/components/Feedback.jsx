537d5243355237efc059628e6a4d9eaa7651f18e
Jagadish6246 <bjp.jagadish@outlook.com>
2025-12-03 23:47:47 +0530

    h3

--- react/first-react-app/src/components/Feedback.jsx
+++ react/first-react-app/src/components/Feedback.jsx
@@ -3,6 +3,7 @@ function Feedback(){
         <div className="border border-2 border-primary p-2 m-2">
             <h1>Feed Back</h1>
             <h2>FeedBack</h2>
+            <h3>Feed Back</h3>
         </div>
     )
 }
